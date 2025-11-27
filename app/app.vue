387fe9b38cef5985dd4bdcb20eb11cd9fea8636a
Riccardo Conventi <work.conventi@gmail.com>
2025-11-27 16:29:17 +0100

    creato il primo componente <FormInput>

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,13 @@
+<script setup lang="ts">
+  import FormInput from './components/FormInput.vue';
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <form>
+      <FormInput class="mb-3" type="text" id="name" label="Nome"></FormInput>
+      <FormInput type="email" id="email" label="Indirizzo email"></FormInput>
+      <input type="submit"></input>
+    </form>
   </div>
 </template>
