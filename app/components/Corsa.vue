259f6224c716d1af4bd95b4cf5a2e2f75d4510b5
Gabriele Pozza <gabriele.pozza@unitn.it>
2025-11-28 17:26:15 +0100

    creazione componente singola corsa con tutti i dati necessari

---
+++ app/components/Corsa.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="tuple-card">
+    <span class="location"> 
+        <i class="bi bi-house-door-fill"></i> 
+          {{ partenza }}  
+      <i class="bi bi-arrow-right"></i>
+        <i class="bi bi-flag-fill"></i> 
+          {{ arrivo }}
+    </span>
+    <div class="time-info">
+      <span class="item">
+        <i class="bi bi-calendar-event"></i>
+        {{ data.toLocaleDateString() }}
+      </span>
+
+      <span class="item">
+        <i class="bi bi-clock"></i>
+        {{ data.toLocaleTimeString() }}
+      </span>
+    </div>
+    <span>
+      {{ stimaKm }} km 
+    </span>
+    <span v-for="e in extra">
+      {{ e.label }}:  {{ e.Number }}
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.tuple-card {
+  width: 100%;
+  padding: 1rem;
+  justify-content: space-between;
+  align-items: center;
+  border: 1.5px solid #ccc;
+  background: #e5e5e5; 
+  border-radius: 15px;
+  display: flex;
+  padding: 1rem;
+}
+
+.location {
+  font-weight: bold;
+  font-size: 1.1rem;
+}
+
+.time-info {
+  display: flex;
+  gap: 12px;
+}
+
+.item {
+  display: flex;
+  align-items: center;
+  gap: 6px;
+}
+</style>
+
+<script>
+  export default {
+    name: "Corsa",
+    props:{
+      partenza: String,
+      arrivo: String,
+      data: Date,
+      stimaKm: Number,
+      extra: Array
+    }
+  }
+  
+</script>
--- app/pages/comune/corse.vue
+++ app/pages/comune/corse.vue
@@ -1,8 +1,50 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Corsa from '~/components/Corsa.vue';
+const extra1 =[
+  {label: "ID utente", Number: "12345"},
+  {label: "ID vettore", Number: "54321"}
+]
+const extra2 =[
+  {label: "ID utente", Number: "12345"},
+]
+const extra3 =[
+  {label: "targa veicolo", Number: "RF132DF"},
+  {label: "ID utente", Number: "12345"}
+]
+</script>
 
 <template>
   <div>
-    Page: comune/corse
+    <Corsa 
+      partenza="Via Rosmini"
+      arrivo="Via Francoforte" 
+      :data="new Date('2025-03-12T15:00')" 
+      :stimaKm="12.5"  
+      :extra="extra1" />
+  </div>
+  <div>
+    <Corsa 
+      partenza="Via Rosmini" 
+      arrivo="Via Francoforte" 
+      :data="new Date('2025-03-12T15:00')" 
+      :stimaKm="12.5"  
+      :extra="extra2" />
+  </div>
+  <div>
+    <Corsa 
+      partenza="Via Rosmini" 
+      arrivo="Via Francoforte" 
+      :data="new Date('2025-03-12T15:00')" 
+      :stimaKm="12.5"  
+      :extra="extra3" />
+  </div>
+  <div>
+    <Corsa 
+      partenza="Via Rosmini" 
+      arrivo="Via Francoforte" 
+      :data="new Date('2025-03-12T15:00')" 
+      :stimaKm="12.5"  
+      :extra="extra1" />
   </div>
 </template>
 
